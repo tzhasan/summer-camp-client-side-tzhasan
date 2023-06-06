a115f4359ba813dd80c6d03af8077b20424305bd
tzhasansohan <sohantajbiul210@gmail.com>
2023-06-07 00:50:32 +0400

    firebase setup done

---
+++ src/Firebase/config.firbase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBSPYjgcWJjiPnsjqgo9jWrLmSkX-4fYGY",
+  authDomain: "summer-camp-project-519c5.firebaseapp.com",
+  projectId: "summer-camp-project-519c5",
+  storageBucket: "summer-camp-project-519c5.appspot.com",
+  messagingSenderId: "367958002581",
+  appId: "1:367958002581:web:ec6cc54eda86e58667449c",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import {
+  GoogleAuthProvider,
+   getAuth,
+  
+} from "firebase/auth";
+import app from '../Firebase/config.firbase';
 
-const AuthProvider = () => {
+export const AuthContext = createContext(null)
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  
+
+useEffect(() => {
+  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+    setUser(currentUser);
+    setLoading(false);
+  });
+  return () => {
+    return unsubscribe();
+  };
+
+  
+}, []);
+  const authInfo = {
+    user
+  }
   return (
-    <div>
-      
-    </div>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
